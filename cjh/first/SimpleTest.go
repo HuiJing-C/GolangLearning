e938af32b623793545f9233c21de00ef5daa9aef
CJH <916864248@qq.com>
2021-04-27 22:59:38 +0800

    test

--- cjh/first/SimpleTest.go
+++ cjh/first/SimpleTest.go
@@ -1,12 +1,36 @@
 package main
 
+/*
+author:cjh
+*/
+// my first golang project
 import (
 	"fmt"
 	"os"
 )
 
+const PI = 3.14
+
+type INT int64
+
+type Student struct {
+	Name string
+	Age  uint8
+}
+
+func AddAndDelete(a, b int8) (c, d int8) {
+	c = a + b
+	d = a - b
+	return
+}
+
 func main() {
 	student := &Student{"zhangsan", 18}
-	student.name = "lisi"
-	fmt.Printf("PID : %d %d %v", os.Getpid(), Add(1, 2), student)
+	student.Name = "lisi"
+	add, _ := AddAndDelete(1, 2)
+	format := fmt.Sprintf("PID : %d %d %v %f", os.Getpid(), add, student, PI)
+
+	fmt.Println(format)
+	var tmpInt INT = 10
+	fmt.Println(tmpInt)
 }
