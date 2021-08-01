099d301674811ef1bedaa9f82786d8e4ec57ecc4
wb-cjh663673 <wb-cjh663673@alibaba-inc.com>
2021-08-01 19:51:43 +0800

    struct and method end

--- cjh/07struct_and_method/02create_by_factory_test.go
+++ cjh/07struct_and_method/02create_by_factory_test.go
@@ -23,7 +23,8 @@ func TestFactory(t *testing.T) {
 	fmt.Printf("%v\n", peo) // &{cjh 12}
 }
 
-// 如果想强制使用工厂方法，使结构提变成私有的。可以运用可见性规则
+// 如果想强制使用工厂方法，使结构体变成私有的。可以运用可见性规则
+// 通过应用可见性规则，就可以禁止使用 new() 函数，强制用户使用工厂方法，从而使类型变成私有的，就像在面向对象语言中那样。
 type peop struct {
 	name string
 	age  int
@@ -32,3 +33,36 @@ type peop struct {
 func NewPeop(name string, age int) *peop {
 	return &peop{"cjh", 18}
 }
+
+// make只适用于三种类型，切片slice，字典map，管道channel
+// 与new()的区别可以从下面的案例区分
+type Foo map[string]string
+type Bar struct {
+	thingOne string
+	thingTwo int
+}
+
+func TestNewAndMake(t *testing.T) {
+	// OK
+	y := new(Bar)
+	(*y).thingOne = "hello"
+	(*y).thingTwo = 1
+
+	// NOT OK
+	// z := make(Bar) // 编译错误：cannot make type Bar
+	// (*y).thingOne = "hello"
+	// (*y).thingTwo = 1
+
+	// OK
+	x := make(Foo)
+	x["x"] = "goodbye"
+	x["y"] = "world"
+
+	// NOT OK
+	u := new(Foo)         // 返回的一个指向nil的指针，他尚未被分配内存
+	(*u)["x"] = "goodbye" // 运行时错误!! panic: assignment to entry in nil map
+	(*u)["y"] = "world"
+}
+
+// 试图 make() 一个结构体变量，会引发一个编译错误，这还不是太糟糕，但是 new() 一个映射并试图使用数据填充它，将会引发运行时错误！
+// 因为 new(Foo) 返回的是一个指向 nil 的指针，它尚未被分配内存。所以在使用 map 时要特别谨慎。
--- cjh/07struct_and_method/ODE.go
+++ cjh/07struct_and_method/ODE.go
@@ -3,3 +3,8 @@ package struct_and_method
 func AAA() {
 	println(666)
 }
+
+type BBB struct {
+	Name string
+	Age  int
+}
