0a1ece23411456fa978ab1536a0923755867d4b2
wb-cjh663673 <wb-cjh663673@alibaba-inc.com>
2021-08-11 19:35:40 +0800

    package two

--- cjh/05map/03for_range_test.go
+++ cjh/05map/03for_range_test.go
@@ -13,24 +13,24 @@ func TestForRange(t *testing.T) {
 	}
 	// one === 1
 	// two === 2
-	fmt.Printf("%v\n", m) // 05map[one:1 two:2]  for range 内部是实际值的拷贝，改变其值并不能改变map
+	fmt.Printf("%v\n", m) // map[one:1 two:2]  for range 内部是实际值的拷贝，改变其值并不能改变map
 }
 
-// 如果想创建一个map类型的切片，要使用两次make，第一次make切片，第二次make 05map
+// 如果想创建一个map类型的切片，要使用两次make，第一次make切片，第二次make map
 func TestMake(t *testing.T) {
 	slice1 := make([]map[string]string, 5)
 	for i := range slice1 {
 		slice1[i] = make(map[string]string)
 		slice1[i]["one"] = "1"
 	}
-	fmt.Printf("%v\n", slice1) // [05map[one:1] 05map[one:1] 05map[one:1] 05map[one:1] 05map[one:1]]
+	fmt.Printf("%v\n", slice1) // [map[one:1] map[one:1] map[one:1] map[one:1] map[one:1]]
 
 	slice2 := make([]map[string]string, 5)
 	for _, v := range slice2 {
 		v = make(map[string]string) // 不能这样赋值，因为v只是实际值的拷贝
 		v["one"] = "1"
 	}
-	fmt.Printf("%v\n", slice2) // [05map[] 05map[] 05map[] 05map[] 05map[]]
+	fmt.Printf("%v\n", slice2) // [map[] map[] map[] map[] map[]]
 }
 
-// map内部既不是按key排序的也不是按value排序的，如果想对map排序，可以先将key赋给一个切片，把切片排序，使用切片的值来取mao的值
+// map内部既不是按key排序的也不是按value排序的，如果想对map排序，可以先将key赋给一个切片，把切片排序，使用切片的值来取map的值
