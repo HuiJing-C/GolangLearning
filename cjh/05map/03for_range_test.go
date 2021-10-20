e0ef207bb1d1c0c972c7855102a65e8e95519dfe
wb-cjh663673 <wb-cjh663673@alibaba-inc.com>
2021-10-20 16:48:29 +0800

    map rang trap

--- cjh/05map/03for_range_test.go
+++ cjh/05map/03for_range_test.go
@@ -34,3 +34,61 @@ func TestMake(t *testing.T) {
 }
 
 // map内部既不是按key排序的也不是按value排序的，如果想对map排序，可以先将key赋给一个切片，把切片排序，使用切片的值来取map的值
+
+type student struct {
+	Name string
+	Age  int
+}
+
+// map rang的一个坑
+func TestMapRange(t *testing.T) {
+	// 定义map
+	m := make(map[string]*student)
+
+	// 定义student数组
+	stus := []student{
+		{Name: "zhou", Age: 24},
+		{Name: "li", Age: 23},
+		{Name: "wang", Age: 22},
+	}
+
+	// 将数组依次添加
+	for _, stu := range stus { // stu是同一个地址里的值，只不过在每次循环时被赋予了不同的值，所以每次&stu都是指向同一个地址，所以遍历完后m的所有value都是wang
+		fmt.Printf("%p %s\n", &stu, stu.Name) // %p打印后为同一个地址
+		m[stu.Name] = &stu                    // 指针拷贝
+	}
+
+	// 打印map
+	for k, v := range m {
+		fmt.Println(k, "=>", v.Name)
+	}
+	// zhou => wang
+	// li => wang
+	// wang => wang
+
+	println("========================以上是错误的示例=======================")
+
+	// >>>>>>>>>>>> 可以这样改
+	m2 := make(map[string]student)
+	for _, stu := range stus { // 虽然stu还是同一个地址的不同值，但是下一行是进行的值拷贝，所以并没有将地址信息带过去
+		m2[stu.Name] = stu
+	}
+
+	for k, v := range m2 {
+		fmt.Println(k, "=>", v.Name)
+	}
+	// zhou => zhou
+	// li => li
+	// wang => wang
+
+	println("======================第二种方法====================")
+	// >>>>>>>>> 也可以这样改
+	m3 := make(map[string]student)
+	for i := 0; i < len(stus); i++ {
+		m3[stus[i].Name] = stus[i]
+	}
+
+	for k, v := range m2 {
+		fmt.Println(k, "=>", v.Name)
+	}
+}
