f5345cd6d8f5958a08e759b7666caf40da5fc293
wb-cjh663673 <wb-cjh663673@alibaba-inc.com>
2021-12-01 11:29:35 +0800

    add insertion sort finish

--- cjh/algorithm/sort/insertionsort_test.go
+++ cjh/algorithm/sort/insertionsort_test.go
@@ -6,15 +6,28 @@ import (
 )
 
 func TestInsertion(t *testing.T) {
-	s := []int{9, 7, 8, 6, 5, 4, 3, 2, 1}
-	fmt.Printf("===%v\n", s)
+	s := []int{9, 7, 8, 4, 6, 5, 0, 2, 1, 3}
+	fmt.Printf("开始%v\n", s)
 	for i := 1; i < len(s); i++ {
-		for j := range s[:i] {
-			if s[i] < s[j] {
-				s = append(append(s[:j], s[i]), s[i+1:]...)
+		current := s[i]
+		for j := 0; j <= i; j++ {
+			if current <= s[j] {
+				moveAfterIndex(s[:i+1], j)
+				s[j] = current
+				break
 			}
 		}
-		fmt.Printf("%v\n", s)
+		fmt.Printf("第%d次排序结果%v\n", i, s)
 	}
-	fmt.Printf("===%v\n", s)
+	fmt.Printf("结果%v\n", s)
+}
+
+func moveAfterIndex(s []int, index int) {
+	l := len(s)
+	if l < 2 || index > l-1 {
+		return
+	}
+	for i := l; i > index && i > 1; i-- {
+		s[i-1] = s[i-2]
+	}
 }
